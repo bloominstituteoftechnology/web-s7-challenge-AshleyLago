893aa5f06ff4899463db8deed24c4851e68efbc4
Ashley Lago <ashl3ylag0@gmail.com>
2024-05-26 01:28:42 -0400

    Finished Project

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react'
+import * as yup from 'yup';
+import axios from 'axios';
 
-// 👇 Here are the validation errors you will use with Yup.
 const validationErrors = {
   fullNameTooShort: 'full name must be at least 3 characters',
   fullNameTooLong: 'full name must be at most 20 characters',
   sizeIncorrect: 'size must be S or M or L'
 }
 
-// 👇 Here you will create your schema.
+const formSchema = yup.object().shape({
+  fullName: yup.string().trim()
+    .min(3, validationErrors.fullNameTooShort)
+    .max(20, validationErrors.fullNameTooLong),
+  size: yup.string()
+    .oneOf(['S','M','L'], validationErrors.sizeIncorrect)
+})
 
-// 👇 This array could help you construct your checkboxes using .map in the JSX.
 const toppings = [
   { topping_id: '1', text: 'Pepperoni' },
   { topping_id: '2', text: 'Green Peppers' },
@@ -18,44 +24,93 @@ const toppings = [
   { topping_id: '5', text: 'Ham' },
 ]
 
+const initialValues = () => ({ fullName: "", size: "", toppings: []});
+const initialErrors = () => ({ fullName: "", size: ""});
+
 export default function Form() {
+  //STATES
+  const [values, setValues] = useState(initialValues());
+  const [errors, setErrors] = useState(initialErrors());
+  const [enabledSub, setEnabledSub] = useState(false);
+  const [succSer, setSuccSer] = useState();
+  const [failSer, setFailSer] = useState();
+  //EFFECT
+  useEffect(() => {
+    formSchema.isValid(values).then(setEnabledSub)
+  }, [values])
+  //CHANGE
+  const onChange = evt => {
+    let { type, checked, name, value} = evt.target;
+    if (type === 'checkbox') {
+      setValues(prev => ({
+        ...prev,
+        toppings: checked
+          ? [...prev.toppings, name]
+          : prev.toppings.filter(t => t !== name)
+      }));
+    } else {
+      setValues(prev => ({ ...prev, [name]: value }));
+      yup.reach(formSchema, name)
+        .validate(value)
+        .then(() => setErrors(prev => ({ ...prev, [name]: '' })))
+        .catch(err => setErrors(prev => ({ ...prev, [name]: err.errors[0] })));
+    }
+  }
+  //SUBMIT
+  const onSubmit = evt => {
+    evt.preventDefault()
+    axios.post('http://localhost:9009/api/order', values)
+      .then(res => {
+        setValues(initialValues())
+        setSuccSer(res.data.message)
+        setFailSer()
+      })
+      .catch(err => {
+        setFailSer(err.response.data.message)
+        setSuccSer()
+      })
+  }
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2>Order Your Pizza</h2>
-      {true && <div className='success'>Thank you for your order!</div>}
-      {true && <div className='failure'>Something went wrong</div>}
+      {succSer && <div className='success'>{succSer}</div>}
+      {failSer && <div className='failure'>{failSer}</div>}
 
       <div className="input-group">
         <div>
           <label htmlFor="fullName">Full Name</label><br />
-          <input placeholder="Type full name" id="fullName" type="text" />
+          <input placeholder="Type full name" id="fullName" name="fullName" type="text" onChange={onChange} value={values.fullName}/>
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {errors.fullName && <div className='error'>{errors.fullName}</div>}
       </div>
 
       <div className="input-group">
         <div>
           <label htmlFor="size">Size</label><br />
-          <select id="size">
+          <select id="size" name="size" onChange={onChange} value={values.size}>
             <option value="">----Choose Size----</option>
-            {/* Fill out the missing options */}
+            <option value="S">Small</option>
+            <option value="M">Medium</option>
+            <option value="L">Large</option>
           </select>
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {errors.size && <div className='error'>{errors.size}</div>}
       </div>
 
       <div className="input-group">
-        {/* 👇 Maybe you could generate the checkboxes dynamically */}
-        <label key="1">
-          <input
-            name="Pepperoni"
-            type="checkbox"
-          />
-          Pepperoni<br />
-        </label>
+        {toppings.map(topping => (
+          <label key={topping.topping_id}>
+            <input
+              name={topping.topping_id}
+              type="checkbox"
+              onChange={onChange}
+              checked={values.toppings.includes(topping.topping_id)}
+            />
+            {topping.text}<br />
+          </label>
+        ))}
       </div>
-      {/* 👇 Make sure the submit stays disabled until the form validates! */}
-      <input type="submit" />
+      <input type="submit" disabled={!enabledSub}/>
     </form>
   )
 }
--- mvpB.test.js
+++ mvpB.test.js
@@ -13,8 +13,25 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [3] sum(1, 3) // returns 4
     [4] sum('1', 2) // returns 3
     [5] sum('10', '3') // returns 13
-  */
 
+  */
+    describe('Task 1 Test', () => {
+      test('throws an error when empty', () => {
+        expect(() => sum()).toThrow('pass valid numbers');
+      });
+      test('throws an error when a non number is used', () => {
+        expect(() => sum(2, 'seven')).toThrow('pass valid numbers');
+      });
+      test('returns 4', () => {
+        expect(sum(1, 3)).toBe(4);
+      });
+      test('returns 3', () => {
+        expect(sum('1', 2)).toBe(3);
+      });
+      test('returns 13', () => {
+        expect(sum('10', '3')).toBe(13);
+      });
+    });
   /*
   👉 TASK 2 - Integration Testing of HelloWorld component at the bottom of this module
 
@@ -29,9 +46,43 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [5] renders a text that reads "JavaScript is pretty awesome"
     [6] renders a text that includes "javaScript is pretty" (use exact = false)
   */
+    describe('Task 2 Test', () => {
+      test('renders a link that reads Home', () => {
+        render(<HelloWorld/>);
+        const hLink = screen.queryByText('Home');
+        expect(hLink).toBeInTheDocument();
+      });
+      test('renders a link that reads About', () => {
+        render(<HelloWorld/>);
+        const aLink = screen.queryByText('About');
+        expect(aLink).toBeInTheDocument();
+      });
+      test('renders a link that reads Blog', () => {
+        render(<HelloWorld/>);
+        const bLink = screen.queryByText('Blog');
+        expect(bLink).toBeInTheDocument();
+      });
+      test('renders a text that reads The Truth', () => {
+        render(<HelloWorld/>);
+        const tText = screen.queryByText('The Truth');
+        expect(tText).toBeInTheDocument();
+      });
+      test('renders a text that reads JavaScript is pretty awesome', () => {
+        render(<HelloWorld/>);
+        const aText = screen.queryByText('JavaScript is pretty awesome');
+        expect(aText).toBeInTheDocument();
+      });
+      test('renders a text that includes javaScript is pretty', () => {
+        render(<HelloWorld/>);
+        const pText = screen.queryByText('javaScript is pretty', {exact:false});
+        expect(pText).toBeInTheDocument();
+      });
+    });
+  /*
   test('you can comment out this test', () => {
     expect(true).toBe(false)
   })
+  */
 })
 
 function sum(a, b) {
